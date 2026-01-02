1365d3a25b9498cc9555b22668cece5c1b8b023d
Abbas Sadriwala <90386455+Abbassadri786@users.noreply.github.com>
2026-01-02 20:05:44 +0530

    Implement BFS algorithm for graph traversal

---
+++ Graphs/BFS of graph.cpp
@@ -0,0 +1,26 @@
+class Solution {
+  public:
+    vector<int> bfs(vector<vector<int>> &adj) {
+        // code here
+        
+        int vis[adj.size()] = {0};
+        vis[0] = 1;
+        vector<int> bfs;
+        queue<int> q;
+        q.push(0);
+        
+        while(!q.empty()){
+            int node = q.front();
+            q.pop();
+            bfs.push_back(node);
+            
+            for(auto it: adj[node]){
+                if(!vis[it]){
+                    vis[it] = 1;
+                    q.push(it);
+                }
+            }
+        }
+        return bfs;
+    }
+};
