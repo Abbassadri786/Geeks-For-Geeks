17af9c092d1599ad7a9a5d5307fca8866376e883
Abbas Sadriwala <90386455+Abbassadri786@users.noreply.github.com>
2026-01-03 23:30:55 +0530

    Update Directed Graph Cycle.cpp

--- Graphs/Directed Graph Cycle.cpp
+++ Graphs/Directed Graph Cycle.cpp
@@ -39,3 +39,51 @@ class Solution {
         return false;
     }
 };
+
+// 2nd Approach using Kans's Algo by calculating indegree
+class Solution {
+  public:
+    bool isCyclic(int V, vector<vector<int>> &edges) {
+        // Code here
+        vector<int> adj[V];
+        
+        for(auto &e: edges){
+            int u = e[0];
+            int v = e[1];
+            adj[u].push_back(v);
+        }
+        
+        vector<int> indegree(V);
+        
+        for(int i=0; i<V; i++){
+            for(auto it: adj[i]){
+                indegree[it]++;
+            }
+        }
+        
+        queue<int> q;
+        for(int i=0; i<V; i++){
+            if(indegree[i] == 0){
+                q.push(i);
+            }
+        }
+        
+        int cnt = 0;
+        
+        while(!q.empty()){
+            int node = q.front();
+            q.pop();
+            cnt++;
+            
+            for(auto it: adj[node]){
+                indegree[it]--;
+                if(indegree[it] == 0) q.push(it);
+            }
+        }
+        
+        if(cnt == V) return false;
+        
+        return true;
+        
+    }
+};
