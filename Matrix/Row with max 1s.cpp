324c790b2815ed4cc61cdbcd3c41db41ae23b787
Abbas Sadriwala <90386455+Abbassadri786@users.noreply.github.com>
2026-01-22 15:36:53 +0530

    Refactor rowWithMax1s function for efficiency

--- Matrix/Row with max 1s.cpp
+++ Matrix/Row with max 1s.cpp
@@ -1,26 +1,38 @@
-#include <bits/stdc++.h>
-using namespace std;
+// User function template for C++
+class Solution {
+  private:
+        int lowerBound(const vector<int>& arr, int n, int x){
+            int low = 0, high = n - 1;
+    
+            while(low <= high){
+                int mid = low + (high - low) / 2;
+                if(arr[mid] >= x){
+                    high = mid - 1;
+                }
+                else{
+                    low = mid + 1;
+                }
+            }
+            return low;
+    }
+  public:
+    int rowWithMax1s(vector<vector<int>> &arr) {
+        // code here
+        int n = arr.size();
+        int m = arr[0].size();
+        int cntMax = 0;
+        int idx = -1;
 
-class Solution{
-public:
-	int rowWithMax1s(vector<vector<int> > arr, int n, int m) {
-	    // code here
-	    int maxCount = 0;
-	    int countRow = -1;
-	    
-	    for(int i=0; i<n; i++){
-	       int count = 0;
-	        for(int j=0; j<m; j++){
-	            if(arr[i][j] == 1){
-	                count++;
-	            }
-	        }
-	        if(maxCount < count){
-	            maxCount = count;
-	            countRow = i;
-	        }
-	    }
-	    return countRow;
+        for(int i = 0; i < n; i++){
+            int firstOneIdx = lowerBound(arr[i], m, 1);
+            int cntOne = m - firstOneIdx;
 
-	}
+            if(cntOne > cntMax){
+                cntMax = cntOne;
+                idx = i;
+            }
+        }
+        return idx;
+        
+    }
 };
