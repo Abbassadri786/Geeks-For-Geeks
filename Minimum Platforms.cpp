24518aeeeb5be68e87b74002fed2923438c23080
Abbas Sadriwala <90386455+Abbassadri786@users.noreply.github.com>
2026-02-09 23:59:34 +0530

    Add minPlatform function to calculate train platforms
    Implement minPlatform function to calculate minimum platforms needed for train schedules.

---
+++ Minimum Platforms.cpp
@@ -0,0 +1,25 @@
+class Solution {
+  public:
+    int minPlatform(vector<int>& arr, vector<int>& dep) {
+        // code here
+        int n = arr.size();
+        sort(arr.begin(), arr.end());
+        sort(dep.begin(), dep.end());
+        int i=0, j=0;
+        int cnt = 0, maxCount = 0;
+        
+        while(i < n){
+            if(arr[i] <= dep[j]){
+                cnt++;
+                i++;
+            }
+            else{
+                cnt--;
+                j++;
+            }
+            maxCount = max(maxCount, cnt);
+        }
+        
+        return maxCount;
+    }
+};
