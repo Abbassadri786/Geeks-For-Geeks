f8a4c1df47293d2c3b0042007e54e5e5b94d0c03
Abbas Sadriwala <90386455+Abbassadri786@users.noreply.github.com>
2026-03-02 05:48:38 +0530

    Create Count Subarray with k odds.cpp

---
+++ Count Subarray with k odds.cpp
@@ -0,0 +1,28 @@
+// Approach : atmost(arr, k) - atMost(arr, k-1)
+class Solution {
+  private:
+    int subArrays(vector<int>& arr, int k){
+        if(k < 0) return 0;
+        int n = arr.size();
+        int r = 0, l = 0, odds = 0, cnt = 0;
+        
+        while( r < n){
+            odds += arr[r] % 2;
+            
+            while(odds > k){
+                odds -= arr[l] % 2;
+                l++;
+            }
+            
+            cnt += (r-l+1);
+            r++;
+        }
+        
+        return cnt;
+    }
+  public:
+    int countSubarrays(vector<int>& arr, int k) {
+        return subArrays(arr, k) - subArrays(arr, k-1);
+        
+    }
+};
