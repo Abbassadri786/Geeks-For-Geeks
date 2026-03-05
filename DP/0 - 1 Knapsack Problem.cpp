ef10d6f24d9f1a2fc51ad22dcc599c02aa218cb3
Abbas Sadriwala <90386455+Abbassadri786@users.noreply.github.com>
2026-03-05 15:04:11 +0530

    Update 0 - 1 Knapsack Problem.cpp

--- DP/0 - 1 Knapsack Problem.cpp
+++ DP/0 - 1 Knapsack Problem.cpp
@@ -37,3 +37,38 @@ public:
         return helper(W, wt, val, n);
     }
 };
+
+
+
+class Solution {
+    // Approach 2:  Using Top Down matrix
+  public:
+    int knapsack(int W, vector<int> &val, vector<int> &wt) {
+        // code here
+        int n = val.size();
+        int dp[n+1][W+1];
+        // Step 1: Initialize matrix using recurssive base conditon
+        for(int i=0; i < n + 1; i++){
+            for(int j=0; j < W + 1; j++){
+                if( i == 0 || j ==0){
+                    dp[i][j] = 0;
+                }
+            }
+        }
+        
+        //Step 2: Iterative solution traverse through matrix and replace n -> i and W -> j of memo sol
+        // Fill up the Dp Table
+        for(int i = 1; i< n + 1; i++){
+            for(int j = 1; j< W + 1; j++){
+                if(wt[i-1] <= j){
+                    dp[i][j] = max(val[i-1] + dp[i-1][j - wt[i-1]] , dp[i-1][j]);
+                }
+                else{
+                    dp[i][j] = dp[i-1][j];
+                }
+            }
+        }
+        
+        return dp[n][W];
+    }
+};
