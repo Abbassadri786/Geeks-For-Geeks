3ad4dd6f4f7a04b165c762eb62b3a509fcd066dc
Abbas Sadriwala <90386455+Abbassadri786@users.noreply.github.com>
2026-03-11 15:35:50 +0530

    Create Shortest Common Supersequence.cpp

---
+++ DP/Shortest Common Supersequence.cpp
@@ -0,0 +1,31 @@
+class Solution {
+    // Shortest Common Supersequence length = (m + n - LCS)
+  public:
+    int minSuperSeq(string &s1, string &s2) {
+        
+        int n = s1.length();
+        int m = s2.length();
+        
+        // dp[i][j] = length of LCS of first i characters of s1 and first j characters of s2
+        vector<vector<int>> dp(n+1, vector<int>(m+1, 0));
+        
+        // Build LCS DP table
+        for(int i = 1; i <= n; i++){
+            for(int j = 1; j <= m; j++){
+                
+                // If characters match → include this character in LCS
+                if(s1[i-1] == s2[j-1]){
+                    dp[i][j] = 1 + dp[i-1][j-1];
+                }
+                else{
+                    // Otherwise take best by skipping one character
+                    dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
+                }
+            }
+        }
+        
+        // Length of shortest common supersequence
+        // = total length of both strings - length of common subsequence
+        return (m + n - dp[n][m]);
+    }
+};
