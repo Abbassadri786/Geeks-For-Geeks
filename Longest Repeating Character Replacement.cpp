3c7e00c1c090c4c20356db4a3c5b14040fb3672e
Abbas Sadriwala <90386455+Abbassadri786@users.noreply.github.com>
2026-04-17 23:33:50 +0530

    Update Longest Repeating Character Replacement.cpp

--- Longest Repeating Character Replacement.cpp
+++ Longest Repeating Character Replacement.cpp
@@ -2,23 +2,32 @@ class Solution {
   public:
     int characterReplacement(string& s, int k) {
         int n = s.length();
-        int l = 0, r = 0, maxF = 0, maxLen = 0;
-        int hash[26] = {0};
+        
+        int l = 0, r = 0;
+        int maxF = 0;     // max frequency of any character in current window
+        int maxLen = 0;   // answer
+        
+        int hash[26] = {0}; // frequency array for uppercase letters
         
         while(r < n){
+            // include current character in window
             hash[s[r] - 'A']++;
+            
+            // update max frequency in current window
             maxF = max(maxF, hash[s[r] - 'A']);
             
-            if((r-l+1) - maxF > k ){
+            // if replacements needed > k, shrink window
+            if((r - l + 1) - maxF > k){
                 hash[s[l] - 'A']--;
                 l++;
             }
             
+            // update maximum valid window length
             maxLen = max(maxLen, r - l + 1);
-            r++;
+            
+            r++; // expand window
         }
         
         return maxLen;
-        
     }
 };
