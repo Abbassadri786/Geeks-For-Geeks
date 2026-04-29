319fd5fb8c93793706cec4f2d758255a44efe998
Abbas Sadriwala <90386455+Abbassadri786@users.noreply.github.com>
2026-04-30 00:34:03 +0530

    Implement serialization and deserialization of binary tree

---
+++ Serialize and deserialize a binary tree.cpp
@@ -0,0 +1,84 @@
+/*
+class Node {
+public:
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int val) {
+        data = val;
+        left = right = nullptr;
+    }
+};
+*/
+
+class Solution {
+  public:
+  
+    // Serialize using BFS (level-order)
+    vector<int> serialize(Node *root) {
+        vector<int> ans;
+        if(!root) return ans;
+
+        queue<Node*> q;
+        q.push(root);
+
+        while(!q.empty()){
+            Node* node = q.front();
+            q.pop();
+
+            if(node == NULL){
+                ans.push_back(-1); // use -1 as NULL marker (safe assumption in GFG)
+            }
+            else{
+                ans.push_back(node->data);
+                q.push(node->left);
+                q.push(node->right);
+            }
+        }
+        
+        return ans;
+    }
+
+    // Deserialize using BFS
+    Node *deSerialize(vector<int> &arr) {
+        if(arr.size() == 0) return NULL;
+
+        Node* root = new Node(arr[0]);
+        queue<Node*> q;
+        q.push(root);
+
+        int i = 1; // start from next element
+
+        while(!q.empty() && i < arr.size()){
+            Node* node = q.front();
+            q.pop();
+
+            // Left child
+            if(arr[i] == -1){
+                node->left = NULL;
+            }
+            else{
+                Node* leftNode = new Node(arr[i]);
+                node->left = leftNode;
+                q.push(leftNode);
+            }
+            i++;
+
+            // Right child
+            if(i < arr.size()){
+                if(arr[i] == -1){
+                    node->right = NULL;
+                }
+                else{
+                    Node* rightNode = new Node(arr[i]);
+                    node->right = rightNode;
+                    q.push(rightNode);
+                }
+                i++;
+            }
+        }
+
+        return root;
+    }
+};
